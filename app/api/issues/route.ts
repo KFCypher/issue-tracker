222e9f21b95e92213dab38c64d91693bda6acfaf
Kelvin <thekelvinfordjour@gmail.com>
2025-11-05 11:50:54 +0000

    Build the new issue page

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -14,9 +14,9 @@ const createIssueSchema = z.object({
 export async function POST(request: NextRequest){
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
-  if (!validation.success) {
+  if (!validation.success) 
     return NextResponse.json(validation.error.issues, { status: 400});
-  }
+  
   const newIssue = await prisma.issue.create({
     data: { title: body.title, description: body.description }
   });
