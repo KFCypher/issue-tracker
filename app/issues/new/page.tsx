b0d4d4b9563512537e617254cd09240cf12964dd
Kelvin <thekelvinfordjour@gmail.com>
2025-11-08 08:26:11 +0000

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,57 @@
 'use client';
 
 import { TextField, Button } from '@radix-ui/themes';
-import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
+import dynamic from 'next/dynamic';
 import "easymde/dist/easymde.min.css";
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Callout } from '@radix-ui/themes';
+import { InfoCircledIcon } from '@radix-ui/react-icons';
+
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+  ssr: false,
+});
+
+interface IssueForm{
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const {register, control, handleSubmit} = useForm<IssueForm>();
+  const router = useRouter();
+  const [error, setError] = useState('');
 
   return (
-    <div className='max-w-xl space-y-3'>
-      <TextField.Root placeholder="Title" >
-        <TextField.Slot />
-      </TextField.Root>
-      <SimpleMDE placeholder="Description" />
-      <Button>Submit New Issue</Button>
+    <div className='max-w-xl'>
+      {error && (
+        <Callout.Root color='red' className='mb-5'>
+          <Callout.Icon>
+		        <InfoCircledIcon />
+	        </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>)}
+      <form className='space-y-3' 
+        onSubmit={handleSubmit(async (data) => {
+        try {
+          await axios.post('/api/issues', data);
+          router.push('/issues');
+        } catch (error) {
+          setError('An Unexpected Error Occurred');
+        }
+        })}>
+        <TextField.Root placeholder="Title" {...register('title')}>
+          <TextField.Slot />
+        </TextField.Root>
+        <Controller name="description" control={control} render={({field}) => (
+        <SimpleMDE placeholder="Description" value={field.value}
+          onChange={field.onChange} />
+        )}/>
+        <Button>Submit New Issue</Button>
+      </form>
     </div>
   )
 }
