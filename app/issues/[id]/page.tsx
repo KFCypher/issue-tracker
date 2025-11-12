f2d90f7309925885e390c13ad11a00a4650c908a
Kelvin <thekelvinfordjour@gmail.com>
2025-11-12 09:40:19 +0000

    Style the issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import prisma from '@/prisma/client'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import React from 'react'
 
@@ -12,8 +14,6 @@ const IssueDetailPage = async ({ params }: Props) => {
   // Check if id is a valid number
   if (isNaN(parseInt(id))) notFound();
 
-  //if (typeof params.id !== 'number') notFound();
-
   const issue = await prisma.issue.findUnique ({
     where: { id: parseInt(id) }
   })
@@ -23,10 +23,14 @@ const IssueDetailPage = async ({ params }: Props) => {
 
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex className='space-x-3' my='2'>
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+        <p>{issue.description}</p>
+      </Card>
     </div>
   )
 }
