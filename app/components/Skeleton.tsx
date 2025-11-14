a7a688e652e9120456e7555191f04bd0c187365c
Kelvin <thekelvinfordjour@gmail.com>
2025-11-14 09:39:12 +0000

    Refactor: Organize import statements

---
+++ app/components/Skeleton.tsx
@@ -0,0 +1,9 @@
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+// ✅ Default export
+const IssueSkeleton = () => {
+  return <div>...</div>
+}
+
+export default Skeleton;
--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,8 +1,5 @@
-import { Box, Card, Flex, Heading } from '@radix-ui/themes'
-import React from 'react'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-import ReactMarkdown from 'react-markdown'
+import { Skeleton } from '@/app/components';
+import { Box, Card, Flex } from '@radix-ui/themes';
 
 const LoadingIssueDetailPage = () => {
   return (
