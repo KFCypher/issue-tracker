a48578b9a3c905341b554cde9c263ec5636b4bea
Kelvin <thekelvinfordjour@gmail.com>
2025-11-16 08:24:20 +0000

    Update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -43,6 +43,9 @@ const IssueForm = ({issue}: {issue?: Issue}) => {
   const onSubmit = handleSubmit(async (data) => {
         try {
           setSubmitting(true);
+          if (issue)
+            await axios.patch(`/api/issues/` + issue.id, data);
+          else
           await axios.post('/api/issues', data);
           router.push('/issues');
         } catch (error) {
@@ -71,7 +74,7 @@ const IssueForm = ({issue}: {issue?: Issue}) => {
           onChange={field.onChange} />
         )}/>
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner/>}</Button>
+        <Button disabled={isSubmitting}>{ issue ? 'Update Issue' : 'Submit New Issue'} {''} {isSubmitting && <Spinner/>}</Button>
       </form>
     </div>
   )
