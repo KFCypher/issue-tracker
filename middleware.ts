43bf37093e54fd059ab0763ab6fe9343836646fb
Kelvin <thekelvinfordjour@gmail.com>
2025-11-21 03:57:19 +0000

    middleware fixes

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,23 @@
-export { default } from 'next-auth/middleware';
+/*export { default } from 'next-auth/middleware';
 
 export const config = {
   matcher: [
     '/issues/new', 
     '/issues/edit/:id+'
     ]
+};*/
+
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
+
+export const config = {
+  matcher: [
+    "/issues/new",
+    "/issues/edit/:id+",
+  ],
 };
