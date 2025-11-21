236f62b8cdf4f70827778b6d220b51bd257a76e7
Kelvin <thekelvinfordjour@gmail.com>
2025-11-21 12:20:41 +0000

    Refactor: Assignee Select Component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -6,47 +6,54 @@ import { Select } from '@radix-ui/themes';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { User } from 'next-auth';
-import toast, { Toaster } from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast';
 
 const AssigneeSelect = ({issue}: {issue: Issue}) => {
-  const {data: users, error, isLoading } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => axios.get('/api/users').then(res => res.data),
-    staleTime: 60 * 1000, // 60s
-    retry: 3
-  });
+  const {data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton />;
 
   if (error) return null;
 
+  const assignIssue = (userId: string) => {
+    axios
+      .patch('/api/issues/' + issue.id, { 
+        assignedToUserId: userId === 'unassigned' ? null : userId 
+      })
+      .catch(() => {
+        toast.error('Changes could not be saved.');
+      });
+  };
+
   return (
     <>
       <Select.Root 
-      defaultValue={issue.assignedToUserId || "unassigned"}
-      onValueChange={async(userId) => {
-         axios
-         .patch('/api/issues/' + issue.id, { 
-            assignedToUserId: userId === 'unassigned' ? null: userId 
-         })
-         .catch(() => {
-          toast.error('Changes could not be saved.');
-         })
-      }}>
+        defaultValue={issue.assignedToUserId || "unassigned"}
+        onValueChange={assignIssue}
+      >
         <Select.Trigger placeholder='Assign...' />
         <Select.Content>
           <Select.Group>
             <Select.Label>Suggestions</Select.Label>
             <Select.Item value="unassigned">Unassigned</Select.Item>
-            {users?.map(user => (
-              <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
             ))}
           </Select.Group>
         </Select.Content>
       </Select.Root>
       <Toaster />
     </>
-  )
-}
+  );
+};
 
-export default AssigneeSelect
+const useUsers = () => useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then(res => res.data),
+    staleTime: 60 * 1000, // 60s
+    retry: 3
+  });
+
+export default AssigneeSelect;
