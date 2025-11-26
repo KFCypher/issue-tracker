55f0101374e7753ae0749d2c98aea7b9978363ab
Kelvin <thekelvinfordjour@gmail.com>
2025-11-26 13:46:37 +0000

    Build the latestIssue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-interface Props {
-  searchParams: Promise<{ page?: string }>;
-}
+//interface Props {
+  //searchParams: Promise<{ page?: string }>;
+//}
 
-export default async function Home({searchParams}: Props) {
+export default async function Home(/*{searchParams}: Props*/) {
 
-  const resolvedSearchParams = await searchParams;
+  //const resolvedSearchParams = await searchParams;
 
-  const pageNumber = parseInt(resolvedSearchParams.page || "1") || 1;
+  //const pageNumber = parseInt(resolvedSearchParams.page || "1") || 1;
 
   return (
-    <Pagination itemCount={100} pageSize={10} currentPage={pageNumber} />
+    //<Pagination itemCount={100} pageSize={10} currentPage={pageNumber} />
+    <LatestIssues />
   );
 }
